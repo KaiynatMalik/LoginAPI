26ab011cf8c0e6a681bb1223213a5a4352fa4dea
Kaiynat <62101120+KaiynatMalik@users.noreply.github.com>
2024-01-02 22:41:41 +0500

    Initial Commit

---
+++ model/product.js
@@ -0,0 +1,41 @@
+//defining scheema
+
+const mongoose = require("mongoose");
+
+
+const productScheema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: [true, "Price must be provided"],
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
+
+    rating: {
+        type: Number,
+        default: 4.9,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+    company: {
+        type: String,
+        enum: {
+            values: ["apple", "samsung", "dell"],
+            message: `{VALUE} is not supported`,
+        },
+    },
+
+
+});
+
+module.exports = mongoose.model('Product' , productScheema);
